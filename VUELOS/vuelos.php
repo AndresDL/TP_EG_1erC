35a2c4af73d8874478dea24fb627ad32e591eb53
AndresDL <andres2000adl@gmail.com>
2026-06-21 21:49:18 -0300

    mini tweak

--- VUELOS/vuelos.php
+++ VUELOS/vuelos.php
@@ -101,7 +101,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['crear_vuelo']) && $es
         $asientos = (int)$_POST['asientos'];
         $codAerolinea = $codAerolineaCEO;
         if($_POST['fechaVuelta'] === '0000-00-00'){
-          $fechaVuelta = null;
+          $fechaVuelta = NULL;
         } else {
           $fechaVuelta = $_POST['fechaVuelta'];
         }
